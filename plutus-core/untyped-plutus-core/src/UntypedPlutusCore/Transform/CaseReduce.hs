f221fec189d1d9198cec98c57d0d45a9bbaf4dfb
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2024-03-04 10:17:53 +0000

    WIP

--- plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Transform/CaseReduce.hs
+++ plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Transform/CaseReduce.hs
@@ -9,11 +9,12 @@ import UntypedPlutusCore.Core
 
 import Control.Lens (transformOf, (^?))
 import Data.List.Extras
+import Data.Vector qualified as V
 
 caseReduce :: Term name uni fun a -> Term name uni fun a
 caseReduce = transformOf termSubterms processTerm
 
 processTerm :: Term name uni fun a -> Term name uni fun a
 processTerm = \case
-    Case ann (Constr _ i args) cs | Just c <- cs ^? wix i -> mkIterApp c ((ann,) <$> args)
+    Case ann (Constr _ i args) cs | Just c <- (V.!?) cs (fromIntegral i) -> mkIterApp c ((ann,) <$> (V.toList args))
     t                                                     -> t
